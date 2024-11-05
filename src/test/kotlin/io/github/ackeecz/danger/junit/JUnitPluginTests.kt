2b3ccc54c4aca73658c97f9d6ce8fb55208bb883
David Brunow <david@shepherddog.co>
2024-11-05 17:31:02 -0600

    Make test more comprehensive

--- src/test/kotlin/io/github/ackeecz/danger/junit/JUnitPluginTests.kt
+++ src/test/kotlin/io/github/ackeecz/danger/junit/JUnitPluginTests.kt
@@ -17,6 +17,30 @@ class JUnitPluginTests {
         // then
         Truth.assertThat(dangerContext.markdowns)
             .isNotEmpty()
+        Truth.assertThat(dangerContext.markdowns.first().toString())
+            .isEqualTo(
+                """
+                    Violation(message=### Tests: 
+    
+                    #### cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest
+                    
+                    | File | Name |
+                    | ---- | ---- |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail with topics section of max topics and no expanded button |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail without similar articles |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail with similar articles |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | display not downloaded info text when saved article and article dont have text |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail with topics section expanded |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail without image |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail with topics section and expanded button |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail without topics section |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should show article detail with topics section of less than max topics and no expanded button |
+                    | cz.flashsport.saved.screens.articledetail.detail.ArticleDetailControllerTest | should handle empty message and type parameters |
+                    
+                    
+                    , file=null, line=null)
+                """.trimIndent()
+            )
     }
 
     @Test
